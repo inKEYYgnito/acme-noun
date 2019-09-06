50a2fe3f27a0f69a6adbfa5caa79453d74ffc9d3
Kristina Cayetano <kma.cayetano@gmail.com>
2019-09-05 21:36:58 -0400

    included apis

--- server.js
+++ server.js
@@ -2,4 +2,51 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const db = require('./db/db')
+const { People, Place, Thing } = db.models
+
+const logger = (req, res, next) => {
+    console.log(`Received ${req.method} request on ${req.url}`)
+    next()
+}
+
+app.use(logger)
+
+app.get('/', (req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, 'index.html'))
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.get('/api/people', async (req, res, next) => {
+    try {
+        console.log('USRRRRRRRRR------>', await People.findAll())
+        res.send()
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.get('/api/places', async (req, res, next) => {
+    try {
+        res.send(await Place.findAll())
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.get('/api/things', async (req, res, next) => {
+    try {
+        res.send(await Thing.findAll())
+    } catch (e) {
+        next(e)
+    }
+})
+
 db.setup()
+    .then(() => {
+        const port = process.env.PORT || 3000
+        app.listen(port, () => console.log('Listening on port', port))
+    }
+)
